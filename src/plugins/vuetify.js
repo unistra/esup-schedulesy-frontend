21dc5630cffaea3a2eae5b965cc629e75bf94c93
Yoran Hillion <yhillion@unistra.fr>
2019-08-30 10:52:24 +0200

    ➕ Vuetify and Axios dependencies added

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import fr from 'vuetify/es5/locale/fr';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    lang: {
+      locales: { fr },
+      current: 'fr',
+    },
+  icons: {
+    iconfont: 'mdi',
+  },
+});
