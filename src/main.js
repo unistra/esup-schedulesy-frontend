cf2129b71e708cbb4c340ee310e1fd55b8eac115
Yoran Hillion <yhillion@unistra.fr>
2019-08-30 11:48:09 +0200

    👷 Sentry integrated

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue';
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
+Sentry.init({
+  dsn: 'https://74d930ab848d4d40a99c2e8326d5d079@sentry-test.app.unistra.fr/23',
+  release: VERSION,
+  environment: process.env.VUE_APP_DEPLOYMENT_ENV,
+  integrations: [
+    new Integrations.Vue({
+      Vue,
+      attachProps: true,
+    }),
+  ],
+});
+
 new Vue({
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
+const webpack = require('webpack');
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
+
+const gitRevisionPlugin = new GitRevisionPlugin();
+
 module.exports = {
   lintOnSave: false,
+  configureWebpack: {
+    plugins: [
+      new GitRevisionPlugin({
+        branch: true,
+      }),
+      new webpack.DefinePlugin({
+        VERSION: JSON.stringify(gitRevisionPlugin.version()),
+        COMMITHASH: JSON.stringify(gitRevisionPlugin.commithash()),
+        BRANCH: JSON.stringify(gitRevisionPlugin.branch()),
+      }),
+    ],
+  },
 };
