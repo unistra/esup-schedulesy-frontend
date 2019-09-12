8c6a90164ae2ee8334ef6bb2ee658ce2ca762620
Yoran Hillion <yhillion@unistra.fr>
2019-09-12 09:11:16 +0200

    🔥 Quickfix for http to https removed as it's done by the backend.

--- src/mixins/childrenEntryGenerator.js
+++ src/mixins/childrenEntryGenerator.js
@@ -4,8 +4,7 @@ export default {
       const childrenEntry = { children: [] };
       const addSelectable = item => ({ ...item, ...{ selectable: true } });
       const addChildren = item => (item.has_children ? { ...item, ...childrenEntry } : item);
-      const normalizeId = item => (process.env.VUE_APP_DEPLOYMENT_ENV !== 'dev' ? { ...item, ...{ id: item.id.replace('http', 'https') } } : item);
-      return normalizeId(addChildren(addSelectable(node)));
+      return addChildren(addSelectable(node));
     },
     sortChildren(childrenList) {
       return childrenList.sort((a, b) => a.name > b.name);
