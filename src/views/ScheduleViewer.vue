73f8f7fd59c6ceeb93ccf5b1c32453962844cd96
Yoran Hillion <yhillion@unistra.fr>
2019-10-04 17:22:31 +0200

    🎨 Reindenting

--- src/views/ScheduleViewer.vue
+++ src/views/ScheduleViewer.vue
@@ -21,122 +21,122 @@
                          @change-type="setType">
       </viewer-toolbar-sm>
       <v-sheet height="600">
-      <v-calendar locale="fr-FR"
-                  ref="calendar"
-                  v-model="focus"
-                  color="primary"
-                  :type="type"
-                  :weekdays="weekdays"
-                  :now="today"
-                  :interval-height="intervalHeight"
-                  first-interval="14"
-                  interval-minutes="30"
-                  interval-count="26"
-                  :events="events"
-                  :event-name="eventName"
-                  :event-color="getEventColor"
-                  event-text-color="secondary"
-                  :interval-format="intervalFormat"
-                  @click:event="showEvent"
-                  @click:date="viewDay"
-                  @click:more="viewDay"
-                  @change="updateRange">
-      </v-calendar>
-      <v-menu
-        v-model="selectedOpen"
-        :close-on-content-click="false"
-        :activator="selectedElement"
-        offset-x>
-        <v-card
-          color="grey lighten-4"
-          min-width="250"
-          flat>
-          <v-toolbar
-            :color="selectedEvent.color"
-            light>
-            <v-toolbar-title >
-              <span>{{ selectedEvent.name }}</span>
-            </v-toolbar-title>
-            <div class="flex-grow-1"></div>
-            <v-btn icon @click="selectedOpen = false">
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-          </v-toolbar>
-          <v-card-text class="pa-0">
-            <v-list dense>
-              <v-list-item v-if="selectedEvent.trainees && selectedEvent.trainees > 0">
-                <v-list-item-icon>
-                  <v-icon>mdi-account-group</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <p class="ma-0">
-                    <span>
-                      {{ eventsTrainees[selectedEvent.trainees[0]].name }}
-                    </span>
-                    <span v-for="(trainee, index) in selectedEvent.trainees.slice(1)"
-                          :key="index">
-                      - {{ eventsTrainees[trainee].name }}
-                    </span>
-                  </p>
-                </v-list-item-content>
-              </v-list-item>
-              <v-list-item v-for="instructor in selectedEvent.instructors"
-                           :key="instructor">
-                <v-list-item-icon>
-                  <v-icon>mdi-account</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <span>{{ eventsInstructors[instructor].name }}</span>
-                </v-list-item-content>
-              </v-list-item>
-              <v-sheet v-for="classroom in selectedEvent.classrooms"
-                       :key="classroom">
-                <v-list-item v-if="eventsClassrooms[classroom].genealogy.length > 0">
+        <v-calendar locale="fr-FR"
+                    ref="calendar"
+                    v-model="focus"
+                    color="primary"
+                    :type="type"
+                    :weekdays="weekdays"
+                    :now="today"
+                    :interval-height="intervalHeight"
+                    first-interval="14"
+                    interval-minutes="30"
+                    interval-count="26"
+                    :events="events"
+                    :event-name="eventName"
+                    :event-color="getEventColor"
+                    event-text-color="secondary"
+                    :interval-format="intervalFormat"
+                    @click:event="showEvent"
+                    @click:date="viewDay"
+                    @click:more="viewDay"
+                    @change="updateRange">
+        </v-calendar>
+        <v-menu
+          v-model="selectedOpen"
+          :close-on-content-click="false"
+          :activator="selectedElement"
+          offset-x>
+          <v-card
+            color="grey lighten-4"
+            min-width="250"
+            flat>
+            <v-toolbar
+              :color="selectedEvent.color"
+              light>
+              <v-toolbar-title >
+                <span>{{ selectedEvent.name }}</span>
+              </v-toolbar-title>
+              <div class="flex-grow-1"></div>
+              <v-btn icon @click="selectedOpen = false">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+            </v-toolbar>
+            <v-card-text class="pa-0">
+              <v-list dense>
+                <v-list-item v-if="selectedEvent.trainees && selectedEvent.trainees > 0">
                   <v-list-item-icon>
-                    <v-icon>mdi-home-map-marker</v-icon>
+                    <v-icon>mdi-account-group</v-icon>
                   </v-list-item-icon>
                   <v-list-item-content>
                     <p class="ma-0">
                       <span>
-                        {{ eventsClassrooms[classroom].genealogy[0] }}
+                        {{ eventsTrainees[selectedEvent.trainees[0]].name }}
                       </span>
-                      <span v-if="eventsClassrooms[classroom].genealogy.length > 1">
-                        - {{ eventsClassrooms[classroom].genealogy[1] }}
+                      <span v-for="(trainee, index) in selectedEvent.trainees.slice(1)"
+                            :key="index">
+                        - {{ eventsTrainees[trainee].name }}
                       </span>
                     </p>
                   </v-list-item-content>
                 </v-list-item>
-                <v-list-item>
+                <v-list-item v-for="instructor in selectedEvent.instructors"
+                             :key="instructor">
                   <v-list-item-icon>
-                    <v-icon>mdi-map-marker</v-icon>
+                    <v-icon>mdi-account</v-icon>
                   </v-list-item-icon>
                   <v-list-item-content>
-                    <span>{{ eventsClassrooms[classroom].name }}</span>
+                    <span>{{ eventsInstructors[instructor].name }}</span>
                   </v-list-item-content>
                 </v-list-item>
-              </v-sheet>
-              <v-list-item v-if="selectedEvent.start">
-                <v-list-item-icon>
-                  <v-icon>mdi-clock-outline</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <p class="ma-0">
-                    <span>{{ selectedEvent.start.length > 10 ? selectedEvent.start.substring(11) : selectedEvent.start }}</span>
-                    <span v-if="selectedEvent.end"> - {{
-                      selectedEvent.start.length > 10 ?
-                      selectedEvent.end.length > 10 ?
-                      selectedEvent.end.substring(11)
-                      : selectedEvent.end
-                      : selectedEvent.end
-                      }}
-                    </span>
-                  </p>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
-          </v-card-text>
-        </v-card>
-      </v-menu>
+                <v-sheet v-for="classroom in selectedEvent.classrooms"
+                         :key="classroom">
+                  <v-list-item v-if="eventsClassrooms[classroom].genealogy.length > 0">
+                    <v-list-item-icon>
+                      <v-icon>mdi-home-map-marker</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <p class="ma-0">
+                        <span>
+                          {{ eventsClassrooms[classroom].genealogy[0] }}
+                        </span>
+                        <span v-if="eventsClassrooms[classroom].genealogy.length > 1">
+                          - {{ eventsClassrooms[classroom].genealogy[1] }}
+                        </span>
+                      </p>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-icon>
+                      <v-icon>mdi-map-marker</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <span>{{ eventsClassrooms[classroom].name }}</span>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-sheet>
+                <v-list-item v-if="selectedEvent.start">
+                  <v-list-item-icon>
+                    <v-icon>mdi-clock-outline</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <p class="ma-0">
+                      <span>{{ selectedEvent.start.length > 10 ? selectedEvent.start.substring(11) : selectedEvent.start }}</span>
+                      <span v-if="selectedEvent.end"> - {{
+                        selectedEvent.start.length > 10 ?
+                        selectedEvent.end.length > 10 ?
+                        selectedEvent.end.substring(11)
+                        : selectedEvent.end
+                        : selectedEvent.end
+                        }}
+                      </span>
+                    </p>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card-text>
+          </v-card>
+        </v-menu>
       </v-sheet>
     </core-section>
   </v-flex>
