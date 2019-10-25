372f75b449ad8cea645af9118eb1e7dd1bcea30f
Yoran Hillion <yhillion@unistra.fr>
2019-10-25 11:26:19 +0200

    🔥 Removing unnecessary code

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,13 +1,9 @@
 import jwt_decode from 'jwt-decode';
 
-const getCookie = (name) => {
-  const v = document.cookie.match(`(^|;) ?${name}=([^;]*)(;|$)`);
-  return v ? v[2] : null;
-};
 export default {
   namespaced: true,
   state: {
-    accessToken: localStorage.getItem('JWT__access__token') || getCookie('jwt_refresh') || '',
+    accessToken: localStorage.getItem('JWT__access__token') || '',
   },
   getters: {
     isLogged: state => state.accessToken.length > 0,
