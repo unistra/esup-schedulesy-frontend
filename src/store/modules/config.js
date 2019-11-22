0f87e68d21360156e5ed63403f48ee8f6a0465b6
Yoran Hillion <yhillion@unistra.fr>
2019-11-22 10:18:21 +0100

    🎨 Better code separation for the store

--- src/store/modules/config.js
+++ src/store/modules/config.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import { forgeResourcesRoot, childrenEntryGenerator } from '@/utils';
+import { forgeResourcesRoot, childrenEntryGenerator } from '@/utils/store';
 
 const resourceTypes = {
   trainee: 'Etudiants',
