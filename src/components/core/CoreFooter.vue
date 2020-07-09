f37c7299745c55f3daaa9ff9c8314287cb2201b2
Yoran Hillion <yhillion@unistra.fr>
2020-07-09 09:03:59 +0200

    💄 CoreFooter: version number addded

--- src/components/core/CoreFooter.vue
+++ src/components/core/CoreFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer>
     <v-spacer></v-spacer>
-    <span>{{ year }} - <strong> Université de Strasbourg</strong> - Tous droits réservés</span>
+    <span>{{ year }} - <strong> Université de Strasbourg</strong> - {{ version }} - Tous droits réservés</span>
     <v-spacer></v-spacer>
   </v-footer>
 </template>
@@ -11,6 +11,7 @@ export default {
   name: 'CoreFooter',
   data: () => ({
     year: new Date().getFullYear(),
+    version: VERSION,
   }),
 };
 </script>
