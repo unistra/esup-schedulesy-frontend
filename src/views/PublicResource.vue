f1d8e0b108543cc21bb4c831947a75bb67a851a2
Yoran Hillion <yhillion@unistra.fr>
2022-09-08 17:21:02 +0200

    🐛 Better leaflet integration with vuetify dialog component

--- src/views/PublicResource.vue
+++ src/views/PublicResource.vue
@@ -279,8 +279,10 @@ export default {
       nativeEvent.stopPropagation();
     },
     showMap(classroom) {
-      this.selectedEventGeolocation = this.eventsClassrooms[classroom].geolocation;
       this.showEventMap = !this.showEventMap;
+      this.$nextTick(() => {
+        this.selectedEventGeolocation = this.eventsClassrooms[classroom].geolocation;
+      })
     },
   },
   mounted() {
